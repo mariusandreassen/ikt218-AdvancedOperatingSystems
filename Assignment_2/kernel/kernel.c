e602597547b16f46876d77320b97756ade78e6b5
Marius Andreassen <mariusnan@uia.no>
2025-03-31 19:46:38 +0200

    Added the final parts to the second assignment.

--- Assignment_2/kernel/kernel.c
+++ Assignment_2/kernel/kernel.c
@@ -1,5 +1,11 @@
 #include "gdt.h"
 
+__attribute__((section(".multiboot")))
+const unsigned int multiboot_header[] = {
+        0x1BADB002,     // magic number
+        0x00,           // flags
+        -(0x1BADB002)   // checksum (magic + flags + checksum == 0)
+};
 
 void terminal_write(const char* str);
 
